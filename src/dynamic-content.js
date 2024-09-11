da6c703526106bfcfbed890f3221df48923d19b9
danielflores2 <daniel.flores@adharma.es>
2024-09-11 15:28:51 +0200

    deslizamiento horizontal a través de los videos

--- src/dynamic-content.js
+++ src/dynamic-content.js
@@ -53,6 +53,75 @@ function init() {
     );
   });
 
+    // Función para inicializar el swipe solo en versión móvil
+    function initSwipeVideos() {
+      const videos = [
+          {
+              src: 'https://static.stratebi.com/proyectos/temp/Myzz-Mala_1080_h264_45000kbs.mp4',
+              href: '/proyectos/myzz_mala.html',
+              text: '<span style="font-size: 13px;">Music Video</span><br><b>Myzz - Mala</b>'
+          },
+          {
+              src: 'https://static.stratebi.com/proyectos/temp/Myzz-Medusa_1080_h264_45000kbs.mp4',
+              href: '/proyectos/myzz_medusa.html',
+              text: '<span style="font-size: 13px;">Music Video</span><br><b>Myzz - Medusa</b>'
+          },
+          {
+              src: 'https://static.stratebi.com/proyectos/temp/Duality_1080_h264_45000kbs.mp4',
+              href: '/proyectos/duality.html',
+              text: '<span style="font-size: 13px;">Commercial</span><br>Duality</b>'
+          },
+          {
+              src: 'https://static.stratebi.com/proyectos/temp/Jaydime_Jaycas_crosscheck_DEFINIT_1.mp4',
+              href: '/proyectos/crosscheck.html',
+              text: '<span style="font-size: 13px;">Music Video</span><br>Cross Check</b>'
+          }
+      ];
+
+      let currentVideoIndex = 0;
+      const videoHome = document.getElementById('videoHome');
+
+      function changeVideoByIndex(index) {
+          const videoData = videos[index];
+          videoHome.src = videoData.src;
+          document.getElementById('overlay-text').innerHTML = videoData.text;
+          videoHome.play();
+      }
+
+      // Detectar eventos táctiles
+      let startX = 0;
+      let startY = 0;
+
+      videoHome.addEventListener('touchstart', (e) => {
+          startX = e.touches[0].clientX;
+          startY = e.touches[0].clientY;
+      });
+
+      videoHome.addEventListener('touchend', (e) => {
+          const endX = e.changedTouches[0].clientX;
+          const endY = e.changedTouches[0].clientY;
+          const diffX = startX - endX;
+          const diffY = startY - endY;
+
+          // Detecta swipe solo si la distancia horizontal es mayor que la vertical
+          if (Math.abs(diffX) > Math.abs(diffY) && Math.abs(diffX) > 50) {
+              if (diffX > 0) {
+                  // Swipe hacia la izquierda, mostrar siguiente video
+                  currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+              } else {
+                  // Swipe hacia la derecha, mostrar video anterior
+                  currentVideoIndex = (currentVideoIndex - 1 + videos.length) % videos.length;
+              }
+              changeVideoByIndex(currentVideoIndex);
+          }
+      });
+  }
+
+  // Inicializa el swipe solo en móvil
+  if (window.innerWidth <= 992) {
+      initSwipeVideos();
+  }
+
 }
 
 document.addEventListener("DOMContentLoaded", init);
