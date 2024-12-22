b96372064347166cbdd72c73f05e63403079e017
danielflores2 <daniel.flores@adharma.es>
2024-12-22 19:16:09 +0100

    Revert "mierdas varias"
    This reverts commit fb9798ab3155e32c244d04ee6b80a01a13cc7cf9.

--- src/scroll.js
+++ src/scroll.js
@@ -9,18 +9,17 @@ function init() {
   let touchEndY = 0;
 
   // FUNCIÓN QUE CONTROLA EL NAV
- function handleScroll() {
-  if (currentIndex > 0) {
-    nav.classList.add('sticky-navbar');
-    nav.classList.remove('position-absolute', 'bottom-0');
-  } else {
-    nav.classList.remove('sticky-navbar');
-    nav.classList.add('position-absolute', 'bottom-0');
+  function handleScroll() {
+    if (currentIndex > 0) {
+      nav.classList.add('sticky-navbar');
+      nav.classList.remove('position-absolute', 'bottom-0');
+    } else {
+      nav.classList.remove('sticky-navbar');
+      nav.classList.add('position-absolute', 'bottom-0');
+    }
   }
-}
 
-   // FUNCION QUE CONTROLA EL SCROLL
-
+   // Función para desplazarse a una sección específica
    function scrollToSection(index) {
     if (index >= 0 && index < sections.length) {
       isScrolling = true;
