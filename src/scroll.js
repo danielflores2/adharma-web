34234c4097071bf7399e2f82bcd91a66b03bcd93
danielflores2 <daniel.flores@adharma.es>
2024-12-24 18:22:31 +0100

    i

--- src/scroll.js
+++ src/scroll.js
@@ -29,16 +29,6 @@ function init() {
     }
   }
 
- function handleScroll() {
-  if (currentIndex > 0) {
-    nav.classList.add('sticky-navbar');
-    nav.classList.remove('position-absolute', 'bottom-0');
-  } else {
-    nav.classList.remove('sticky-navbar');
-    nav.classList.add('position-absolute', 'bottom-0');
-
-  }
-}
 
   // Función para desplazarse a una sección específica
   function scrollToSection(index) {
