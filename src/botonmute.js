a113898eae50f910b62b2bc6e4e3f7393e22d046
danielflores2 <daniel.flores@adharma.es>
2025-01-11 21:47:22 +0100

    boton mute en movil

--- src/botonmute.js
+++ src/botonmute.js
@@ -1,3 +1,5 @@
+
+//BOTON ORDENADOR
 const soundButton = document.getElementById('soundButton');
 
 // Función para alternar el sonido en todos los videos
@@ -22,4 +24,31 @@ function toggleSound() {
 
 // Asignar el evento al botón
 soundButton.addEventListener('click', toggleSound);
-soundButton.addEventListener('touchstart', toggleSound);  // Para dispositivos móviles
+
+
+
+//BOTON MOVIL
+const soundButtonVertical = document.getElementById('soundButtonVertical');
+
+// Función para alternar el sonido en todos los videos
+function toggleSoundVertical() {
+    const videos = document.querySelectorAll('video'); // Seleccionamos todos los videos
+
+    videos.forEach(video => {
+        if (video.muted) {
+            video.muted = false; // Desactivar mute
+        } else {
+            video.muted = true; // Activar mute
+        }
+    });
+
+    // Cambiar el texto del botón dependiendo del estado del sonido
+    if (videos[0].muted) {
+        soundButtonVertical.textContent = 'volume_off'; // Icono de sonido apagado
+    } else {
+        soundButtonVertical.textContent = 'volume_up'; // Icono de sonido activado
+    }
+}
+
+// Asignar el evento al botón
+soundButtonVertical.addEventListener('click', toggleSoundVertical);
