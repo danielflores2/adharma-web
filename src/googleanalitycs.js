4d8f23a1f5591873fe4ab4a3f86bac03093f950d
danielflores2 <daniel.flores@adharma.es>
2025-01-11 19:07:37 +0100

    google analitycs

---
+++ src/googleanalitycs.js
@@ -0,0 +1,14 @@
+// Google tag (gtag.js)
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-FFKBMF7DYV";
+    document.head.appendChild(script);
+  
+    script.onload = function() {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-FFKBMF7DYV');
+    };
+  })();
