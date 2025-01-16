b101eb5c811f457bb6f8612dfe8f574ae282b3be
danielflores2 <daniel.flores@adharma.es>
2025-01-16 19:05:02 +0100

    quitar preload

--- src/preload.js
+++ src/preload.js
@@ -10,7 +10,6 @@ if (!isMobileDevice()) {
     const videosToPreload = [
       "https://d17wmwjckt0fmo.cloudfront.net/videos/MagoCarga.mp4",
       "https://d17wmwjckt0fmo.cloudfront.net/videos/reel2024.mov",
-      "https://d17wmwjckt0fmo.cloudfront.net/videos/adharma__the_magician's_cut__-_demo_reel_2024%20(1080p).mp4",
       "https://d17wmwjckt0fmo.cloudfront.net/videos/ProyectosMP4/Duality_1080_h264_45000kbs.mp4",
       "https://d17wmwjckt0fmo.cloudfront.net/videos/ProyectosMP4/Myzz-Mala_1080_h264_45000kbs.mp4",
       "https://d17wmwjckt0fmo.cloudfront.net/videos/ProyectosMP4/Myzz-Medusa_1080_h264_45000kbs.mp4",
